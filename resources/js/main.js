79a143d2b2e2dd3c1ed0cf01c3ef1ed6a5cc754e
Tasnim Ahmed <58226075+tasnimahmed20@users.noreply.github.com>
2021-01-03 04:47:44 +0600

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 50,
+        time: 5000
+    });
+    new WOW().init();
+});
+new CircleType(document.getElementById('demo2'))
+    .dir(-1)
+    .radius(384);
